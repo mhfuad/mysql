8cd4409b737e9fee81ffefe7bbf2b2d35c5a6986
mhfuad <mhfuad2@gmail.com>
2022-03-14 17:00:17 +0600

    self join

---
+++ 197.self-join.sql
@@ -0,0 +1,23 @@
+SELECT 
+    CONCAT(m.lastName, ', ', m.firstName) AS Manager,
+    CONCAT(e.lastName, ', ', e.firstName) AS 'Direct report'
+FROM
+    employees e
+INNER JOIN employees m ON 
+    m.employeeNumber = e.reportsTo
+ORDER BY 
+    Manager;
+
+-- MySQL self join using LEFT JOIN clause
+
+SELECT 
+    IFNULL(
+        CONCAT(m.lastname, ', ', m.firstname), 'Top Manager'
+        ) AS 'Manager',
+    CONCAT(e.lastname, ', ', e.firstname) AS 'Direct report'
+FROM
+    employees e
+LEFT JOIN employees m ON 
+    m.employeeNumber = e.reportsto
+ORDER BY 
+    manager DESC;
